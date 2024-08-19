46084d3c1205609131840227255b2ce8bef552d8
hrjtju <gravitastju@gmail.com>
2024-08-20 00:17:32 +0800

    Fixing interaction with JoyRL module

--- slm_env.py
+++ slm_env.py
@@ -3,7 +3,8 @@ import random
 from typing import Any, Dict, Tuple, List, Literal
 import torch
 from torch import Tensor as Tensor
-# import joyrl
+import joyrl
+from gymnasium import Env, spaces
 
 from slm_classes import load_json_to_class
 from batch_solution import Batch, Solution
@@ -13,7 +14,7 @@ from batch_solution import Batch, Solution
 
 # only supports assigning a part to a batch
 # and the 2D bin packing algorithm puts the part into a target position
-class SLMEnv:
+class SingleSLMEnv(Env):
     """
     Environment of SLM Machine Optimization
     """
@@ -45,28 +46,31 @@ class SLMEnv:
         
         random.seed(seed)
         
+        self.name = "SingleSLMEnv"
         self.phase = phase
         self.in_path = in_path
         self.view_shape = view_shape
+        self.max_part_type = max_part_type
+        self.max_orientation_num = max_orientation_num
         
         # randomly pick a json file in the training dir
         # and pack the training data into a class
         if self.phase == "Train":
             load_path = random.choice(os.listdir(self.in_path))
-            self.metadata = load_json_to_class(os.path.join(self.in_path, load_path))
+            self.slm_metadata = load_json_to_class(os.path.join(self.in_path, load_path))
             
         # if the phase is Test, choose the file indicated by the path.
         elif self.phase == "Test":
             # load the specified in_path
-            self.metadata = load_json_to_class(self.in_path)
+            self.slm_metadata = load_json_to_class(self.in_path)
             
         # Raise Error if self.phase is not among the two strings above.
         else:
             raise NotImplementedError
         
-        # pass the max params to self.metadata for generating state matrix and mask matrix
-        self.metadata.max_part_type = max_part_type
-        self.metadata.max_orientation_num = max_orientation_num
+        # pass the max params to self.slm_metadata for generating state matrix and mask matrix
+        self.slm_metadata.max_part_type = max_part_type
+        self.slm_metadata.max_orientation_num = max_orientation_num
         
         # -----------------------------------------------------------
         # transform the loaded data into states
@@ -77,24 +81,33 @@ class SLMEnv:
         self.last_state = None
         
         # Get the shape of the working area
-        self.L = self.metadata.machine.build_l
-        self.W = self.metadata.machine.build_w
-        self.H = self.metadata.machine.build_h
+        self.L = self.slm_metadata.machine.build_l
+        self.W = self.slm_metadata.machine.build_w
+        self.H = self.slm_metadata.machine.build_h
         
         # Create an instance of class Solution and initialize it by calling .add_batch() method.
         self.solution = Solution(view_shape=self.view_shape)
-        self.solution.add_batch(self.metadata.machine,
-                                self.metadata.process)
+        self.solution.add_batch(self.slm_metadata.machine,
+                                self.slm_metadata.process)
+        self.view_process_shape = (1, *view_shape)
         
         # observation of the current batch
         self.curr_state = (
-            self.solution.get_current_view(),        # Current discretized view of the batch, Variable
+            self.solution.get_current_view().reshape(*self.view_process_shape),        # Current discretized view of the batch, Variable
             torch.tensor([self.L, self.W, self.H]),  # Real size of the batch, Constant
-            self.metadata.init_state()               # Situation of all parts, Variable
+            self.slm_metadata.init_state()               # Situation of all parts, Variable
         )
         
         # Initial reference for comparing criterion numbers.
         self.last_criterion = 0
+        
+        # Gymnasium Env Spaces Setup
+        self.observation_space = spaces.Tuple(spaces=[
+            spaces.Box(low=0, high=float("inf"), shape=(1, *view_shape)),
+            spaces.Box(low=0, high=float("inf"), shape=(3,)),
+            spaces.Box(low=0, high=float("inf"), shape=(max_part_type, max_orientation_num))
+        ])
+        self.action_space = spaces.Discrete(max_part_type * max_orientation_num)
     
     def get_unavailable_mask(self):
         """
@@ -102,7 +115,7 @@ class SLMEnv:
             - The remaining number of parts is greater than 0
             - There is at least 1 legal orientation
         """
-        return ((self.state[-1][:, 0] > 0) * (torch.sum(self.metadata.mask_matrix(), dim=-1) > 0)).reshape(-1)
+        return ((self.state[-1][:, 0] > 0) * (torch.sum(self.slm_metadata.mask_matrix(), dim=-1) > 0)).reshape(-1)
     
     # verify physical constraints
     def check_geo_constraints(self) -> float:
@@ -150,7 +163,7 @@ class SLMEnv:
         
         # Create new self.curr_state.
         self.curr_state = (
-            view,
+            view.reshape(*self.view_process_shape),
             self.last_state[1],
             temp_part_state
         )
@@ -180,17 +193,17 @@ class SLMEnv:
         
         # Reshape action vector:
         #   [ max_part_type * max_orientation_num ] --> [ max_part_type, max_orientation_num ]
-        out_matrix = action.reshape(self.metadata.max_part_type, self.metadata.max_orientation_num)
+        out_matrix = action.reshape(self.slm_metadata.max_part_type, self.slm_metadata.max_orientation_num)
         
         # Perform softmax to ensure all the instances are strictly greater than 0
-        feasible_matrix = torch.softmax(out_matrix, dim=None) * self.metadata.mask_matrix()
+        feasible_matrix = torch.softmax(out_matrix, dim=None) * self.slm_metadata.mask_matrix()
         
         # Flag var, Whether the part is allocated successfully
         allocated = False
         
         # Select part_id and orientation_id
         # TODO: 
-        part_id = torch.argmax(torch.sum(feasible_matrix, dim=-1) / torch.sum(self.metadata.mask_matrix(), dim=-1))
+        part_id = torch.argmax(torch.sum(feasible_matrix, dim=-1) / torch.sum(self.slm_metadata.mask_matrix(), dim=-1))
         
         # Get the rank of different orientations of the part selected according to the output score
         orientation_rank = torch.argsort(feasible_matrix[part_id].reshape(-1))
@@ -200,7 +213,7 @@ class SLMEnv:
         orientation_id = orientation_rank[rank_ptr]
         
         # Try allocating the part according to the orientation selected.
-        view, allocated = self.solution.add_part(self.metadata.parts[part_id], orientation=orientation_id)
+        view, allocated = self.solution.add_part(self.slm_metadata.parts[part_id], orientation=orientation_id)
                 
         # If allocation fails, try other orientations
         # If the part still cannot be allocated, then add a new bin and reallocate
@@ -225,7 +238,7 @@ class SLMEnv:
                 if feasible_matrix[part_id, orientation_id] == 0:
                     break
                     
-                view, allocated = self.solution.add_part(self.metadata.parts[part_id], orientation=orientation_id)
+                view, allocated = self.solution.add_part(self.slm_metadata.parts[part_id], orientation=orientation_id)
             
             if allocated:
                 self.update_state(view=view, part_id=part_id)
@@ -237,8 +250,8 @@ class SLMEnv:
                 # reset orientation
                 rank_ptr = 0
                 # Add a new batch (same size) and retry allocating
-                self.solution.add_batch(self.metadata.machine, self.metadata.process)
-                view, allocated = self.solution.add_part(self.metadata.parts[part_id],
+                self.solution.add_batch(self.slm_metadata.machine, self.slm_metadata.process)
+                view, allocated = self.solution.add_part(self.slm_metadata.parts[part_id],
                                                          orientation=orientation_rank[rank_ptr])
                 
                 if allocated:
@@ -258,3 +271,10 @@ class SLMEnv:
         self.last_criterion = criterion
         
         return self.curr_state, reward, terminated, truncated, info
+
+if __name__ == "__main__":
+    print(joyrl.__version__) # print version
+    yaml_path = "./yaml_configurations/SingleSLMEnv-v0.yaml"
+    slm_single_env = SingleSLMEnv(in_path="./instances_json/")
+    joyrl.run(yaml_path=yaml_path, env=slm_single_env)
+
